39806973e5073b2b0f7783c1cf71bf13ad75eec3
Darsyn <122276971+DE-IGNIS@users.noreply.github.com>
2026-08-24 21:40:50 +0530

    PLA: Problem Solved 2009A

---
+++ 01-Math/A2009_Minimize.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class A2009_Minimize {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            System.out.println(b - a);
+        }
+
+        sc.close();
+    }
+}
