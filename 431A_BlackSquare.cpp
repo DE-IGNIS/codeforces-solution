0e0d5308095584ee03f8f13a5a22c9df5d546831
Darsyn <122276971+DE-IGNIS@users.noreply.github.com>
2026-09-15 23:15:49 +0530

    PLA: Problem Sovled 431A

---
+++ 431A_BlackSquare.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a[4];
+    cin >> a[0] >> a[1] >> a[2] >> a[3];
+
+    string s;
+    cin >> s;
+
+    long long total = 0;
+
+    for (char c : s) {
+        total += a[c - '1'];
+    }
+
+    cout << total << '\n';
+
+    return 0;
+}
